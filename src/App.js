7d88e34a97ab478d88986a42e431527c55c3ef87
nakai-tomoki <nakai_tomoki@cyberagent.co.jp>
2019-06-25 11:00:00 +0900

    suppose dynamic image

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import DynamicCarousel from "./DynamicCarousel";
 
 function App() {
-  const contents = [];
+  const [contents, setContents] = useState([
+    "https://farm9.staticflickr.com/8059/28286750501_dcc27b1332_h_d.jpg",
+    "https://farm6.staticflickr.com/5812/23394215774_b76cd33a87_h_d.jpg",
+    "https://farm8.staticflickr.com/7455/27879053992_ef3f41c4a0_h_d.jpg"
+  ]);
+  const [flag, toggle] = useState(true);
+  const onClick = () => {
+    if (flag) {
+      setContents([
+        "https://farm8.staticflickr.com/7455/27879053992_ef3f41c4a0_h_d.jpg",
+        "https://farm8.staticflickr.com/7367/27980898905_72d106e501_h_d.jpg",
+        "https://farm8.staticflickr.com/7356/27980899895_9b6c394fec_h_d.jpg"
+      ]);
+    } else {
+      setContents([
+        "https://farm9.staticflickr.com/8059/28286750501_dcc27b1332_h_d.jpg",
+        "https://farm6.staticflickr.com/5812/23394215774_b76cd33a87_h_d.jpg",
+        "https://farm8.staticflickr.com/7455/27879053992_ef3f41c4a0_h_d.jpg"
+      ]);
+    }
+    toggle(!flag);
+  };
   return (
     <div>
       <DynamicCarousel contents={contents} initialSlide={1} infinite={true}>
-        <img
-          alt="1"
-          src="https://farm9.staticflickr.com/8059/28286750501_dcc27b1332_h_d.jpg"
-        />
-        <img
-          alt="2"
-          src="https://farm6.staticflickr.com/5812/23394215774_b76cd33a87_h_d.jpg"
-        />
-        <img
-          alt="3"
-          src="https://farm8.staticflickr.com/7455/27879053992_ef3f41c4a0_h_d.jpg"
-        />
-        <img
-          alt="4"
-          src="https://farm8.staticflickr.com/7367/27980898905_72d106e501_h_d.jpg"
-        />
-        <img
-          alt="5"
-          src="https://farm8.staticflickr.com/7356/27980899895_9b6c394fec_h_d.jpg"
-        />
+        {contents.map((content, idx) => {
+          return <img alt={idx} src={content} key={`slide${idx}`} />;
+        })}
       </DynamicCarousel>
+      <button onClick={onClick}>ボタン</button>
     </div>
   );
 }
