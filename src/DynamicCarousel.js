b4000ca6a0c15a551b6f752f18f67eb13bfe91e2
nakai-tomoki <nakai_tomoki@cyberagent.co.jp>
2019-07-10 21:04:49 +0900

    ドラッグのスワイプ対応

--- src/DynamicCarousel.js
+++ src/DynamicCarousel.js
@@ -1,45 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { useSwipeable, LEFT, RIGHT } from "react-swipeable";
+import cx from "classname";
 import "./DynamicCarousel.css";
 
 export const SLIDE_ACTION = {
   NOMAL: "NOMAL",
   PREVING: "PREVING",
-  NEXTING: "NEXTING"
+  NEXTING: "NEXTING",
+  RETURNING: "RETURNING"
 };
 
 export default React.memo(
   ({
-    // initialSlide = 1,
     children = [],
     afterChange = () => {},
     isEnd = false,
     isStart = false
   }) => {
-    const [slideState, setSlideState] = useState(SLIDE_ACTION.NOMAL);
+    const [slideActionState, setSlideActionState] = useState(
+      SLIDE_ACTION.NOMAL
+    );
+    const [swipedDeltaX, setSwipedDeltaX] = useState(0);
+    const refBlock = useRef(null);
+    const isSwiping =
+      slideActionState === SLIDE_ACTION.PREVING ||
+      slideActionState === SLIDE_ACTION.NEXTING ||
+      slideActionState === SLIDE_ACTION.RETURNING;
     const slideleStyle = index => {
       const standardMargin = -100 + index * 100;
       let slidingMargin = 0;
-      if (slideState === SLIDE_ACTION.PREVING) {
+      if (slideActionState === SLIDE_ACTION.PREVING) {
         slidingMargin = 100;
-      } else if (slideState === SLIDE_ACTION.NEXTING) {
+      } else if (slideActionState === SLIDE_ACTION.NEXTING) {
         slidingMargin = -100;
       }
       const startMargin = isStart ? 100 : 0;
       return {
-        marginLeft: `${standardMargin + slidingMargin + startMargin}%`
+        marginLeft: `${standardMargin +
+          slidingMargin +
+          startMargin +
+          -swipedDeltaX / 10}%`
       };
     };
+    const handlers = useSwipeable({
+      onSwiping: ({ deltaX }) => setSwipedDeltaX(deltaX),
+      onSwiped: ({ velocity, dir }) => {
+        const isStrongSwipe = velocity > 1;
+        const isSwipedLeft = dir === LEFT;
+        const isSwipedRight = dir === RIGHT;
+        const halfClientWidth = refBlock.current.clientWidth / 2;
+        const isSwipedOverHorizontal =
+          swipedDeltaX < -halfClientWidth || swipedDeltaX > halfClientWidth;
+        const shouldNextSlide =
+          !isEnd && isSwipedLeft && (isSwipedOverHorizontal || isStrongSwipe);
+        const shouldPrevSlide =
+          !isStart &&
+          isSwipedRight &&
+          (isSwipedOverHorizontal || isStrongSwipe);
+        if (shouldNextSlide) {
+          setSlideActionState(SLIDE_ACTION.NEXTING);
+        } else if (shouldPrevSlide) {
+          setSlideActionState(SLIDE_ACTION.PREVING);
+        } else {
+          setSlideActionState(SLIDE_ACTION.RETURNING);
+        }
+        setSwipedDeltaX(0);
+      },
+      preventDefaultTouchmoveEvent: true,
+      trackMouse: true
+    });
     const slides = children.map((content, index) => {
       return (
         <article
-          className="Slide"
+          className={cx("Slide", { "Slide--Swiping": isSwiping })}
           style={slideleStyle(index)}
           key={`slide_${content.key}`}
           onTransitionEnd={() => {
             // 1つ目のスライドにだけtransition終了時のイベントを追加
             if (index === 0) {
-              afterChange(slideState);
-              setSlideState(SLIDE_ACTION.NOMAL);
+              afterChange(slideActionState);
+              setSlideActionState(SLIDE_ACTION.NOMAL);
             }
           }}
         >
@@ -48,23 +88,24 @@ export default React.memo(
       );
     });
     const onClickPrev = () => {
-      setSlideState(SLIDE_ACTION.PREVING);
+      setSlideActionState(SLIDE_ACTION.PREVING);
     };
     const onClickNext = () => {
-      setSlideState(SLIDE_ACTION.NEXTING);
+      setSlideActionState(SLIDE_ACTION.NEXTING);
     };
-    const isSliding =
-      slideState === SLIDE_ACTION.PREVING ||
-      slideState === SLIDE_ACTION.NEXTING;
     return (
       <React.Fragment>
-        <div className="Block">{slides}</div>
+        <div ref={refBlock}>
+          <div className="Block" {...handlers}>
+            {slides}
+          </div>
+        </div>
         <div className="SliderControl">
           {isStart ? null : (
             <button
               className="ButtonPrev"
               onClick={onClickPrev}
-              disabled={isSliding}
+              disabled={isSwiping}
               style={isStart ? { visibility: true } : null}
             />
           )}
@@ -72,7 +113,7 @@ export default React.memo(
             <button
               className="ButtonNext"
               onClick={onClickNext}
-              disabled={isSliding || isEnd}
+              disabled={isSwiping || isEnd}
               style={isEnd ? { visibility: true } : null}
             />
           )}
